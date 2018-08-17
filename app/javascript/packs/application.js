04ebb998a17f95d6d4f5b229a8792826b2ab203f
Beatrice Olivera <beatriceolivera1999@gmail.com>
2018-08-17 16:27:46 +0100

    added even more css and javascript

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,4 +7,36 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 import 'bootstrap';
-console.log('Hello World from Webpacker')
+// console.log('Hello World from Webpacker')
+import anime from 'animejs'
+
+var lineDrawing = anime({
+  targets: 'path',
+  strokeDashoffset: [anime.setDashoffset, 0],
+  easing: 'easeInOutCubic',
+  duration: 4000,
+  begin: function(anim) {
+    document.querySelector('path').setAttribute("stroke", "black");
+    document.querySelector('path').setAttribute("fill", "none");
+  },
+  complete: function(anim) {
+    document.querySelector('path').setAttribute("fill", "none");
+  },
+  autoplay: true
+});
+
+
+$('.ml6 .letters').each(function(){
+  $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+});
+
+anime.timeline({loop: false})
+  .add({
+    targets: '.ml6 .letter',
+    translateY: ["1.0em", 0],
+    translateZ: 0,
+    duration: 2000,
+    delay: function(el, i) {
+      return 50 * i;
+    }
+  });
