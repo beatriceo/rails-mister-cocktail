bc424948d9cc2282f06a4c2714025deffca696ec
Beatrice Olivera <beatriceolivera1999@gmail.com>
2018-08-18 08:33:38 +0100

    added image url validation

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -3,4 +3,5 @@ class Cocktail < ApplicationRecord
   has_many :ingredients, through: :doses
   has_many :reviews
   validates :name, uniqueness: :true, presence: :true
+  validates :image_url, allow_blank: true, format: { with: %r{\Ahttp(s?):\/\/.+(\.(png|jpg|gif|jpeg))\Z}, message: 'must be a URL for GIF, JPG or PNG image.' }
 end
